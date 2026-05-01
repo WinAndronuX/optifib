6d90cf429c1a7b39e778f41abca5780d30070e7a
Ethan <ethanespinoza9@gmail.com>
2026-05-01 15:19:21 -0600

    imprementación del mini heap

--- include/optifib/priority_queue.h
+++ include/optifib/priority_queue.h
@@ -4,6 +4,24 @@
  * Prototipos para implementacion de Min-Heap
  */
 
+#include "graph.h"
 
+
+typedef struct _minheap {
+    int size;
+    int capacity;
+    int* pos;
+    Node** array;
+} MinHeap;
+
+
+MinHeap* createMinHeap(int capacity);
+void minHeapify(MinHeap* minHeap, int idx);
+Node* extractMin(MinHeap* minHeap);
+
+
+void decreaseKey(MinHeap* minHeap, int v, double new_loss);
+bool isInMinHeap(MinHeap* minHeap, int v);
+void freeMinHeap(MinHeap* minHeap);
 
 #endif
