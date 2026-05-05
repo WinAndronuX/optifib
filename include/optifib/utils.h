ae0cb60c96553e715b0f2a47a6f02fe4d4ff62f4
Ethan Espinoza <202354341+BossBucket@users.noreply.github.com>
2026-05-05 15:20:40 -0600

    Revert "Se implementaron nuevas funciones en 'utils.c' para la interfaz en el menu"

--- include/optifib/utils.h
+++ include/optifib/utils.h
@@ -14,7 +14,5 @@
 // Funcion para limpiar la consola
 void clearConsole();
 bool isInRange(int x, int a, int b);
-int clearBuffer();
-int getInt(int min, int max);
-void pausa();
+
 #endif
--- lib/utils.c
+++ lib/utils.c
@@ -4,7 +4,6 @@
 
 #include <stdlib.h>
 #include <optifib/utils.h>
-#include <stdio.h>
 
 void clearConsole() {
     #ifdef _WIN32
@@ -16,28 +15,4 @@ void clearConsole() {
 
 bool isInRange(int x, int a, int b) {
     return x >= a && x <= b;
-}
-
-int clearBuffer() {
-    int c, n = 0;
-    while ((c = getchar()) != '\n' && c != EOF) n++;
-    return n;
-}
-
-int getInt(int min, int max) {
-    int val = 0;
-    while (1) {
-        printf("> ");
-        int r = scanf("%d", &val);
-        int n = clearBuffer();
-        if ((r == 1 && n == 0 ) && (val >= min && val <= max)) {
-            return val;
-        }
-        printf("\t|ERROR| Debe ingresar un numero entre %d y %d.\n", min, max);
-    }
-}
-
-void pausa() {
-    printf("\nOprima [Enter] para continuar...");
-    getchar();
 }
